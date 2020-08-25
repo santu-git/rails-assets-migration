31f42358d0c90787e1075351f0885648297b9a76
Santu Koley <santu.cst08@gmail.com>
2020-08-25 10:51:37 +0530

    Upgrade to rails 5.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Ckeditor::Engine => '/ckeditor'
   resources :posts
   root "posts#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
